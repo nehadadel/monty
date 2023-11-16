36441f1879452ac6459bf44b0a60c90e542eacb9
nehadadel <eng.nehadadel@gmail.com>
2023-11-16 15:17:23 -0800

    puh

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include "monty.h"
 stack_t *head = NULL;
-int main(void)
+int main(int ac, char *av[])
 {
  
-   readlines_from_file("bytecodes/00.m");
+  if (ac != 2)
+    {
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+    }
+  readlines_from_file(av[1]);
    return (0);
 }
