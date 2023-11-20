7334fdc541942e9e944ce3360394f15600d9dcbc
nehadadel <eng.nehadadel@gmail.com>
2023-11-19 16:58:49 -0800

    pstr

--- rot.c
+++ rot.c
@@ -9,14 +9,19 @@ void opcode_rotl(
 __attribute__((unused))stack_t **s, unsigned int l_n)
 {
 int n;
-stack_t *new;
- 
+stack_t *new, *tmp;
+
 if (head == NULL)
 return;
 
 n = head->n;
-opcode_pop(s, l_n);
-
 new = create_new_node(n);
-opcode_push(&new, l_n);
+opcode_pop(s, l_n);
+tmp = head;
+
+while (tmp->next)
+{
+tmp = tmp->next;
+}
+tmp->next = new;
 }
